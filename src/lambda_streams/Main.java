34549c559f9e5fd78d9859455ee56f56df8db2ff
Norita Sieffert <nsieffert@tkocustoms.com>
2020-07-01 10:58:01 -0500

    bonus - converting to js file, and started on animals

--- src/lambda_streams/Main.java
+++ src/lambda_streams/Main.java
@@ -1,23 +1,38 @@
 package lambda_streams;
 
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.List;
 
 import static lambda_streams.Numbers.nums;
+import static lambda_streams.Animals.animals;
 
 public class Main {
 
     public static void main(String[] args) {
-        Numbers numbers = new Numbers();
-        System.out.println(numbers.isEven.test(0));
-        System.out.println(numbers.isOdd.test(3));
-        System.out.println(numbers.isPrime.test(3));
-        System.out.println(numbers.added.compute1(nums));
-        System.out.println(numbers.subtracted.compute1(nums));
-        System.out.println(numbers.multiplied.compute1(BigInteger.ONE));
-        System.out.println(numbers.divided.compute2(3, 5));
-        System.out.println(numbers.findMax.compute1(nums));
-        System.out.println(numbers.findMin.compute1(nums));
-        System.out.println(numbers.compare.compute2(0, 13));
-        System.out.println(numbers.append.compute1(42));
+        //Numbers numbers = new Numbers();
+//        System.out.println(numbers.isEven.test(0));
+//        System.out.println(numbers.isOdd.test(3));
+//        System.out.println(numbers.isPrime.test(3));
+//        System.out.println(numbers.added.compute1(nums));
+//        System.out.println(numbers.subtracted.compute1(nums));
+//        System.out.println(numbers.multiplied.compute1(BigInteger.ONE));
+//        System.out.println(numbers.divided.compute2(3, 5));
+//        System.out.println(numbers.findMax.compute1(nums));
+//        System.out.println(numbers.findMin.compute1(nums));
+//        System.out.println(numbers.compare.compute2(0, 13));
+//        System.out.println(numbers.append.compute1(42));
+
+        Animals animal = new Animals();
+        List<String> animaList = Arrays.asList();
+        System.out.println("original animals : " + animals.toString());
+        System.out.println(animal.capsFirst(animals, false));
+        System.out.println(animal.capsFirst(animaList, true));
+        System.out.println(animal.lowerFirst(animals,false));
+        System.out.println(animal.lowerFirst(animaList,true));
+        //System.out.println(animal.addAnimal("Coyote"));
+        System.out.println(animal.flipAnimals(animaList, true));
+        System.out.println(animal.flipAnimals(animals, false));
+        System.out.println();
     }
 }
--- src/lambda_streams/numbers.js
+++ src/lambda_streams/numbers.js
@@ -1,1 +1,73 @@
-//Bonus: refactor Numbers.java into javascript
+let nums = new Array(10, 100, 1000, 5, 50, 500, 3, 30, 300, 7, 70, 700, 1, 10, 100, 25, 250, 2500);
+
+
+
+    const isOdd = (i) => i % 2 != 0;
+    const isEven = (i) => i % 2 == 0;
+   const isPrime = (i) => i % 2 != 0 && i / i == 1;
+    const added = (nums) => {
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        return sum;
+    };
+    const subtracted = (nums) => {
+        int sub = 2500;
+        for (int i : nums) {
+            sub -= i;
+        }
+        return sub;
+    };
+
+  const multiplied = (n) => {
+        let mult = 1;
+        for (let i = 1; i < nums.length, nums++) {
+            mult = mult.multiply(BigInteger.valueOf(i));
+        }
+        return mult;
+    };
+
+        const divided = (k, j) => {
+            int div = nums.get(j) / nums.get(k);
+            return div;
+        };
+        const findMax = (p) => {
+            int b;
+            int max = 0;
+            for (b = 1; b < nums.size(); b++)
+                if (nums.get(b) > max) {
+                    max = nums.get(b);
+                }
+            return Collections.singletonList(max);
+        };
+
+        const findMin = (d) => {
+            int min = 10;
+            for (int c = 1; c < nums.size(); c++) {
+                if (nums.get(c) < min) {
+                    min = nums.get(c);
+                }
+            }
+            return min;
+        };
+
+        const compare = (l, m) => {
+            l = nums.get(l);
+            m = nums.get(m);
+            if (l > m) {
+                return 1;
+            } else if (m > l) {
+                return -1;
+            } else if (l == m) {
+                return 0;
+            } else {
+                return l;
+            }
+        };
+
+        const append = (a) -> {
+            nums[0] = a;
+            return a;
+        };
+    };
