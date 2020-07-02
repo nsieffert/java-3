01c94f98a30e337c1650dc1b13a4d861af0d4a87
Norita Sieffert <nsieffert@tkocustoms.com>
2020-07-01 20:32:07 -0500

    final work for animals, and I attempted a bonus, but ran out of time. If not yet graded, I will try to finish this.

--- src/lambda_streams/Animals.java
+++ src/lambda_streams/Animals.java
@@ -11,8 +11,6 @@ class Animals {
     static List<String> animals = Arrays.asList("peacoCK", "rabbit", "chiwawa", "OranguTAN", "vipeR", "cobra", "paNDa", "bUffalo", "DeeR", "maLLard");
     List<String> animaList = new ArrayList<>();
     List<String> animaList1 = Arrays.asList("Coyote");
-    List<String> animaList2 = Arrays.asList("Bear", "Chicken", "Cow", "Dog", "Egret", "Frog", "Goat");
-    List<String> animaList3 = Arrays.asList("Sheep");
 
     public static void main(String[] args) {
         System.out.println("original animals : " + animals);
@@ -79,7 +77,7 @@ class Animals {
     oneFunction<List<String>> flipAnimals = animaList -> {
 
         Supplier<Stream<String>> streamSupplier = () -> animals.stream();
-        animals.stream().sorted(Collections.reverseOrder()).forEach((String animal) -> {
+        animals.stream().map(animal -> animal.toLowerCase()).sorted(Collections.reverseOrder()).forEach((String animal) -> {
             System.out.println("Flip animals " + animal);
         });
         return animaList;
@@ -87,25 +85,22 @@ class Animals {
 
     oneFunction<List<String>> sortAnimals = animaList -> {
         Supplier<Stream<String>> streamSupplier = () -> animals.stream();
-        animaList.stream().sorted(Comparator.naturalOrder()).forEach((String animal) -> {
+        animals.stream().map(animal -> animal.toLowerCase()).sorted(Comparator.naturalOrder()).forEach((String animal) -> {
             System.out.println("sort animals " + animal);
         });
-        return animaList;
+        return animals;
     };
-}
 
 
+    //Bonus try: add a new lambda function that adds random animals to an array. Well, it's not random.
+    oneFunction<List<String>> randomAnimals = animals -> {
+        List<String> animaList3 = Arrays.asList("Bear", "Chicken", "Cow", "Dog", "Egret", "Frog", "Goat");
+        Random random = new Random();
+        int a = random.nextInt(animals.size()*6);
+            animaList3 = Stream.concat(animaList3.stream(), animals.stream()).parallel()
+                    .collect(Collectors.toList());
+            return animaList3;
+        };
 
-        //animaList.stream().sorted().forEach(System.out::println);
-        //animals.stream().sorted().collect(Collectors.toList());
-       // animaList.forEach(System.out::println);
-        //System.out.println("Animal List " + animaList);
-
-
-    //Bonus try: add a new lambda function that adds random animals to an array
-//    oneFunction<List<String>> randomAnimals = animaList2 -> {
-//    Random random = new Random();
-//    int index = random.nextInt(animaList2.size());
-//    int a = vals.charAt(index);
-
+    };
 
--- src/lambda_streams/Main.java
+++ src/lambda_streams/Main.java
@@ -10,18 +10,18 @@ import static lambda_streams.Animals.animals;
 public class Main {
 
     public static void main(String[] args) {
-      //  Numbers numbers = new Numbers();
-//        System.out.println(numbers.isEven.test(0));
-//        System.out.println(numbers.isOdd.test(3));
-//        System.out.println(numbers.isPrime.test(3));
-       // System.out.println(numbers.added.compute1(nums));
-//        System.out.println(numbers.subtracted.compute1(nums));
-//        System.out.println(numbers.multiplied.compute1(BigInteger.ONE));
-//        System.out.println(numbers.divided.compute2(3, 5));
-//        System.out.println(numbers.findMax.compute1(nums));
-//        System.out.println(numbers.findMin.compute1(nums));
-//        System.out.println(numbers.compare.compute2(0, 13));
-//        System.out.println(numbers.append.compute1(42));
+        Numbers numbers = new Numbers();
+        System.out.println(numbers.isEven.test(0));
+        System.out.println(numbers.isOdd.test(3));
+        System.out.println(numbers.isPrime.test(3));
+        System.out.println(numbers.added.compute1(nums));
+        System.out.println(numbers.subtracted.compute1(nums));
+        System.out.println(numbers.multiplied.compute1(BigInteger.ONE));
+        System.out.println(numbers.divided.compute2(3, 5));
+        System.out.println(numbers.findMax.compute1(nums));
+        System.out.println(numbers.findMin.compute1(nums));
+        System.out.println(numbers.compare.compute2(0, 13));
+        System.out.println(numbers.append.compute1(42));
 
         Animals animal = new Animals();
         System.out.println("original animals : " + animals.toString());
@@ -36,5 +36,6 @@ public class Main {
         System.out.println("+++++++++++++++++");
         animal.sortAnimals.compute1(animals);
         System.out.println("+++++++++++++++++");
+        System.out.println(animal.randomAnimals.compute1(animals));
     }
 }
